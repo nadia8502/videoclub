dec4bc6b72c422f03f076d0d6e2466a8e594196d
nadia <nadia.janine@gmail.com>
2019-01-16 23:35:48 +0100

    ejercicio 2

---
+++ resources/views/catalog/edit.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Midificar pelicula</h1>
+<?php
+$id = 10;
+ echo e($id); ?>
+</body>
+</html>
---
+++ resources/views/catalog/show.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>lista peliculas</h1>
+<?php
+$id = 10;
+ echo e($id); ?>
+</body>
+</html>
